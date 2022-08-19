e52b405991d826ff3cd6a289e35f876492f4f598
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-08-19 09:57:31 -0300

    helpers: add ones constants for bytes and address

--- packages/helpers/src/constants.ts
+++ packages/helpers/src/constants.ts
@@ -5,5 +5,9 @@ import { bn } from './numbers'
 export const maxUint = (e: number): BigNumber => bn(2).pow(e).sub(1)
 
 export const MAX_UINT256: BigNumber = maxUint(256)
+
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const ONES_ADDRESS = '0xffffffffffffffffffffffffffffffffffffffff'
+
 export const ZERO_BYTES32 = '0x0000000000000000000000000000000000000000000000000000000000000000'
+export const ONES_BYTES32 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
