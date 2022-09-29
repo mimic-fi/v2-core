5db9e9ae8909bbe46c1e5f14bf86a8aab1ed9fd3
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-09-29 18:01:17 -0300

    chore: remove unnecessary task compile overload

--- packages/wallet/hardhat.config.ts
+++ packages/wallet/hardhat.config.ts
@@ -2,13 +2,8 @@ import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-local-networks-config-plugin'
 
-import { overrideFunctions } from '@mimic-fi/v2-helpers'
-import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names'
-import { task } from 'hardhat/config'
 import { homedir } from 'os'
 import path from 'path'
-
-task(TASK_COMPILE).setAction(overrideFunctions(['query']))
 
 export default {
   localNetworksConfig: path.join(homedir(), '/.hardhat/networks.mimic.json'),
