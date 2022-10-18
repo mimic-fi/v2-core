51a4437d3a5dba635d1f9e0bb59383d93df50ca1
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2022-10-18 07:20:38 -0300

    Strategies: Implement AAVE v2 (#3)

---
+++ packages/strategies/contracts/aave-v2/IAaveV2IncentivesController.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.0;
+
+// solhint-disable func-name-mixedcase
+
+interface IAaveV2IncentivesController {
+    /**
+     * @dev for backward compatibility with previous implementation of the Incentives controller
+     */
+    function REWARD_TOKEN() external view returns (address);
+
+    /**
+     * @dev returns the unclaimed rewards of the user
+     * @param user the address of the user
+     * @return the unclaimed user rewards
+     */
+    function getUserUnclaimedRewards(address user) external view returns (uint256);
+
+    /**
+     * @dev Gets the end date for the distribution
+     * @return The end of the distribution
+     **/
+    function getDistributionEnd() external view returns (uint256);
+
+    /**
+     * @dev Claims reward for an user, on all the assets of the lending pool, accumulating the pending rewards
+     * @param assets List of cTokens claiming rewards for
+     * @param amount Amount of rewards to claim
+     * @param to Address that will be receiving the rewards
+     * @return Rewards claimed
+     **/
+    function claimRewards(address[] calldata assets, uint256 amount, address to) external returns (uint256);
+}
---
+++ packages/strategies/test/AaveV2Strategy.mainnet.ts
@@ -0,0 +1,138 @@
+import {
+  advanceTime,
+  currentTimestamp,
+  deploy,
+  fp,
+  getSigner,
+  impersonate,
+  incrementBlocks,
+  instanceAt,
+  YEAR,
+} from '@mimic-fi/v2-helpers'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import { expect } from 'chai'
+import { Contract } from 'ethers'
+import { ethers } from 'hardhat'
+
+/* eslint-disable no-secrets/no-secrets */
+
+const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+const AUSDC = '0xBcca60bB61934080951369a648Fb03DF4F96263C'
+const WHALE = '0x55FE002aefF02F77364de339a1292923A15844B8'
+
+const LENDING_POOL = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9'
+
+describe('AaveV2Strategy - USDC', function () {
+  let strategy: Contract, usdc: Contract, aUsdc: Contract, registry: Contract
+  let whale: SignerWithAddress
+
+  const toUSDC = (amount: number) => fp(amount).div(1e12)
+
+  const DATA = '0x'
+  const SLIPPAGE = 0
+  const JOIN_AMOUNT = toUSDC(50)
+
+  before('impersonate whale', async () => {
+    whale = await impersonate(WHALE, fp(100))
+  })
+
+  before('deploy strategy', async () => {
+    const admin = await getSigner()
+    registry = await deploy('@mimic-fi/v2-registry/artifacts/contracts/registry/Registry.sol/Registry', [admin.address])
+    strategy = await deploy('AaveV2Strategy', [USDC, LENDING_POOL, registry.address])
+  })
+
+  before('load tokens', async () => {
+    usdc = await instanceAt('IERC20', USDC)
+    aUsdc = await instanceAt('IAaveV2Token', await strategy.aToken())
+  })
+
+  it('deploys the strategy correctly', async () => {
+    expect(await strategy.token()).to.be.equal(USDC)
+    expect(await strategy.aToken()).to.be.equal(AUSDC)
+    expect(await strategy.lendingPool()).to.be.equal(LENDING_POOL)
+    expect(await strategy.valueRate()).to.be.equal(fp(1))
+    expect(await strategy.lastValue(strategy.address)).to.be.equal(0)
+
+    expect(await strategy.registry()).to.be.equal(registry.address)
+    expect(await strategy.NAMESPACE()).to.be.equal(ethers.utils.solidityKeccak256(['string'], ['STRATEGY']))
+  })
+
+  it('joins the strategy', async () => {
+    await usdc.connect(whale).transfer(strategy.address, JOIN_AMOUNT)
+
+    const previousUsdcBalance = await usdc.balanceOf(strategy.address)
+    const previousAUsdcBalance = await aUsdc.balanceOf(strategy.address)
+
+    await strategy.join(JOIN_AMOUNT, SLIPPAGE, DATA)
+
+    const currentUsdcBalance = await usdc.balanceOf(strategy.address)
+    expect(currentUsdcBalance).to.be.equal(previousUsdcBalance.sub(JOIN_AMOUNT))
+
+    const currentAUsdcBalance = await aUsdc.balanceOf(strategy.address)
+    expect(currentAUsdcBalance).to.be.equal(previousAUsdcBalance.add(JOIN_AMOUNT))
+  })
+
+  it('accrues value over time', async () => {
+    const previousValue = await strategy.lastValue(strategy.address)
+
+    await advanceTime(YEAR)
+    await incrementBlocks(10)
+
+    const currentValue = await strategy.lastValue(strategy.address)
+    expect(currentValue).to.be.gt(previousValue)
+  })
+
+  it('accrues rewards over time', async () => {
+    const incentivesController = await instanceAt('IAaveV2IncentivesController', await strategy.incentivesController())
+    if ((await currentTimestamp()).gt(await incentivesController.getDistributionEnd())) {
+      console.log('AAVE V2 liquidity mining ended')
+    } else {
+      const rewardsToken = await instanceAt('IERC20', await incentivesController.REWARD_TOKEN())
+      const previousBalance = await rewardsToken.balanceOf(strategy.address)
+
+      await strategy.claim('0x')
+
+      const currentBalance = await rewardsToken.balanceOf(strategy.address)
+      expect(currentBalance).to.be.gt(previousBalance)
+    }
+  })
+
+  it('exits with a 50%', async () => {
+    const previousValue = await strategy.lastValue(strategy.address)
+    const previousUsdcBalance = await usdc.balanceOf(strategy.address)
+    const previousAUsdcBalance = await aUsdc.balanceOf(strategy.address)
+
+    const ratio = fp(0.5)
+    await strategy.exit(ratio, SLIPPAGE, DATA)
+
+    const currentValue = await strategy.lastValue(strategy.address)
+    expect(currentValue).to.be.lt(previousValue)
+
+    // USDC balance should be increased due to the accrued interest
+    const currentUsdcBalance = await usdc.balanceOf(strategy.address)
+    expect(currentUsdcBalance).to.be.gt(previousUsdcBalance.add(JOIN_AMOUNT.div(2)))
+
+    // aUSDC balance should be reduced by the same ratio
+    const currentAUsdcBalance = await aUsdc.balanceOf(strategy.address)
+    const expectedAUsdcBalance = previousAUsdcBalance.mul(fp(1).sub(ratio)).div(fp(1))
+    expect(currentAUsdcBalance).to.be.at.least(expectedAUsdcBalance.sub(1))
+    expect(currentAUsdcBalance).to.be.at.most(expectedAUsdcBalance.add(1))
+  })
+
+  it('exits with a 100%', async () => {
+    const previousUsdcBalance = await usdc.balanceOf(strategy.address)
+
+    const ratio = fp(1)
+    await strategy.exit(ratio, SLIPPAGE, DATA)
+
+    // More than the entire joined amount should be back to the strategy
+    const currentUsdcBalance = await usdc.balanceOf(strategy.address)
+    expect(currentUsdcBalance).to.be.gt(JOIN_AMOUNT)
+    expect(currentUsdcBalance).to.be.gt(previousUsdcBalance.add(JOIN_AMOUNT.div(2)))
+
+    // There should not be any remaining aUSDC balance
+    const currentAUsdcBalance = await aUsdc.balanceOf(strategy.address)
+    expect(currentAUsdcBalance).to.be.equal(0)
+  })
+})
