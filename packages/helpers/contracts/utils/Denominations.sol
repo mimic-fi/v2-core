46bb68d358dcc00abb951e70f07c085e3677ac0d
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2022-11-22 10:27:53 -0300

    Helpers: Implement denominations helpers (#26)

--- packages/helpers/contracts/utils/Denominations.sol
+++ packages/helpers/contracts/utils/Denominations.sol
@@ -21,4 +21,8 @@ pragma solidity ^0.8.0;
  */
 library Denominations {
     address internal constant NATIVE_TOKEN = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    function isNativeToken(address token) internal pure returns (bool) {
+        return token == NATIVE_TOKEN;
+    }
 }
