d97576e48ff10bcd6d673e0976eeb74ce53830ce
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-12-07 21:20:19 -0300

    swap: implement hop amm connector

--- packages/swap-connector/contracts/ISwapConnector.sol
+++ packages/swap-connector/contracts/ISwapConnector.sol
@@ -22,14 +22,15 @@ import '@mimic-fi/v2-registry/contracts/implementations/IImplementation.sol';
  */
 interface ISwapConnector is IImplementation {
     /**
-     * @dev Enum identifying the sources proposed: Uniswap V2, Uniswap V3, Balancer V2, Paraswap V5, and 1inch V5.
+     * @dev Enum identifying the sources proposed: Uniswap V2, Uniswap V3, Balancer V2, Paraswap V5, 1inch V5, and Hop.
      */
     enum Source {
         UniswapV2,
         UniswapV3,
         BalancerV2,
         ParaswapV5,
-        OneInchV5
+        OneInchV5,
+        Hop
     }
 
     /**
