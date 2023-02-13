dee254f204c4dd0afb27c3e7eae43ba57dad137a
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-02-13 17:57:56 -0300

    Smart Vault: Add non-reentrant guards to authorized fns (#50)

--- packages/registry/contracts/implementations/InitializableImplementation.sol
+++ packages/registry/contracts/implementations/InitializableImplementation.sol
@@ -14,7 +14,7 @@
 
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/proxy/utils/Initializable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 
 import './BaseImplementation.sol';
 
